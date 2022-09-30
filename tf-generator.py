180446eb9f9fba848dd61b8c73bae9c97f3c4fc3
Victor Fondevilla <vfondevilla@miteksystems.com>
2022-09-30 17:40:17 +0200

    updating

--- tf-generator.py
+++ tf-generator.py
@@ -2,6 +2,8 @@
 import sys
 import os
 import re
+import hcl2
+import json
 
 
 ## This script will read the Terraform files and create a list of all the variables
@@ -12,7 +14,10 @@ tosearch = 'var.'
 variables = []
 def open_file(filename):
 	with open(filename, 'r') as f:
+		# dict = hcl2.load(f)
+		# print(dict)
 		return f.read()
+		# return dict
 
 ## This function will parse each line of the Terraform file and return a list of all the variables,
 ## the variables can be anywhere on the line
@@ -21,7 +26,7 @@ def parse_variables(file):
 		if tosearch in line:
 			# found = (line.split(tosearch)[1].split()[0] and line.split(tosearch)[1].split("}")[0])
 			found = (re.split('var.', line)[1]) 
-			found = (re.split(r"[^a-zA-Z0-9\s]", found)[0])
+			found = (re.split(r"[^a-zA-Z0-9\s_-]", found)[0])
 			if found not in variables:
 				variables.append(found)
 	return variables
@@ -36,4 +41,5 @@ def print_variables(variables):
 for file in os.listdir(sys.argv[1]):
 	if file.endswith(".tf"):
 		variables = parse_variables(open_file(file))
+		# print(variables)
 print_variables(variables)
